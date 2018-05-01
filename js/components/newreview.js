177654a6f9f58d260c24c3ef7462512e73250614
Ellen Ormerod <ellen.ormerod17@gmail.com>
2018-05-01 16:59:06 -0500

    add new review done

--- js/components/newreview.js
+++ js/components/newreview.js
@@ -1,15 +1,24 @@
 'use strict';
+import { submitReviews } from '../store';
+import { connect } from 'react-redux';
 let React = require('react');
 let t = require('tcomb-form-native');
 let { StyleSheet, Text, View, TouchableHighlight } = require('react-native');
 
 let Form = t.form.Form;
+const Noise = t.enums({
+  1: '1 star-too loud',
+  2: '2 star-loud',
+  3: '3 star-perfect',
+  4: '4 star-quiet',
+  5: '5 star-too quiet'
+});
+
 // here we are: define your domain model
-let Person = t.struct({
-    name: t.String,              // a required string
-    surname: t.maybe(t.String),  // an optional string
-    age: t.Number,               // a required number
-    rememberMe: t.Boolean        // a boolean
+let Review = t.struct({
+    title: t.String,              // a required string
+    comments: t.String,  // an optional string
+    noise: Noise,               // a required number
   });
 let options = {}; // optional rendering options (see documentation)
 
@@ -42,35 +51,52 @@ let styles = StyleSheet.create({
     }
   });
 
-const ReviewForm = ({
-
-  onPress: function () {
+class NewReview extends React.Component {
+  constructor(props){
+    super(props);
+    this.onPress = this.onPress.bind(this)
+  }
+  onPress() {
     // call getValue() to get the values of the form
+    // console.log('this', this)
     const value = this.refs.form.getValue();
     if (value) { // if validation fails, value will be null
-      console.log(value); // value here is an instance of Person
+      const obj = {title: value.title, comments: value.comments, noise: value.noise, placeId: this.props.place}
+      this.props.sendReview(obj); // value here is an instance of Person
     }
-  },
+  }
 
-  render: function() {
+  render() {
     return (
       <View style={styles.container}>
         {/* display */}
         <Form
-          ref="form"
-          type={Person}
+          ref="form" // assign a ref
+          type={Review}
           options={options}
         />
         <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
-          <Text style={styles.buttonText}>Save</Text>
+          <Text style={styles.buttonText}>Submit</Text>
         </TouchableHighlight>
       </View>
     );
   }
-});
-
-const NewReview = () => {
-    return ReviewForm
 }
 
-export default NewReview;
+const mapState = state => {
+  console.log('state', state)
+  return {
+    place: state.reviews[0].placeId
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    sendReview(val){
+      dispatch(submitReviews(val))
+    }
+  }
+}
+
+
+export default connect(mapState, mapDispatch)(NewReview);
--- js/store/reviews.js
+++ js/store/reviews.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
+import NewReview from '../components/newreview';
 
 //Action types
 const GET_REVIEWS = 'GET_REVIEWS';
-
+const NEW_REVIEW = 'NEW_REVIEW'
 //Action creators
 const getReviews = reviews => ({type: GET_REVIEWS, reviews})
-
+const newReview = review => ({type: NEW_REVIEW, review})
 //Thunk creators
 export const fetchReviews = id => dispatch => {
   return axios.get(`http://localhost:8080/api/reviews/${id}`)
-  .then(res => {
-    return dispatch(getReviews(res.data))})
+  .then(res => dispatch(getReviews(res.data)))
   .catch(err => console.error(err))
-  
+}
+
+export const submitReviews = values => dispatch => {
+  return axios.post('http://localhost:8080/api/reviews/', values)
+  .then(res => dispatch(newReview(res.data)))
+  .catch(err => console.error(err))
 }
 
 //Reducer
@@ -20,6 +25,8 @@ export default function(reviews = [], action){
   switch (action.type){
     case GET_REVIEWS:
       return action.reviews
+    case NEW_REVIEW:
+      return [...reviews, action.review]
     default:
       return reviews
   }
